ca3e50aa5539070b8b0fad1c2799141ec367b7f9
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-10-28 15:12:03 +0900

    db test 221028

---
+++ src/main/java/com/smhrd/controller/testDAO.java
@@ -0,0 +1,18 @@
+package com.smhrd.controller;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
+public class testDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	
+	public int testInsert(String test) {
+		SqlSession session = sqlSessionFactory.openSession(true);
+		int row = session.insert("testInsert",test);
+		session.close();
+		
+		return row;
+	}
+}
--- src/main/java/com/smhrd/test/test.java
+++ src/main/java/com/smhrd/test/test.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.controller.testDAO;
+
 public class test extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("test용 서블릿 페이지 입니다.");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("test");
+		
+		String test = request.getParameter("test");
+		
+		System.out.println(test);
+		testDAO dao = new testDAO();
+		int row = dao.testInsert(test);
+		
 	}
 
 }
