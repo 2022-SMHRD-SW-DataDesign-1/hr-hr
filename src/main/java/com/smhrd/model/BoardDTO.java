38b5b4ff5220a011d5643f28e969d29c9562d21b
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-01 15:13:09 +0900

    주석추가

--- src/main/java/com/smhrd/model/BoardDTO.java
+++ src/main/java/com/smhrd/model/BoardDTO.java
@@ -10,15 +10,24 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class BoardDTO {
-
+	
+	// 게시글 번호
 	private int b_num;
+	// 게시글 제목
 	private String b_title;
+	// 게시글 작성자
 	private String b_writer;
+	// 게시글 내용
 	private String b_content;
+	// 게시글 업로드 파일
 	private String b_filename;
+	// 게시글 작성일
 	private Date b_date;
+	// 게시글 좋아요 수
 	private int b_likes;
+	// 게시글 조회 수 
 	private int b_cnt;
+	// 게시글 점수
 	private int b_point;
 	
 	
