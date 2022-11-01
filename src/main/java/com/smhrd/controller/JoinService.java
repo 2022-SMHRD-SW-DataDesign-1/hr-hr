f2f7d901c11910aef178151ec458567c27fe0420
smhrd <smhrd@DESKTOP-TCH56SA>
2022-11-01 16:22:51 +0900

    조인서비스, 로그인서비스, MemberDTO 회원가입 생성자 추가, MemberMapper 회원가입, 로그인 Mapper 추가 -우경

--- src/main/java/com/smhrd/controller/JoinService.java
+++ src/main/java/com/smhrd/controller/JoinService.java
@@ -1,14 +1,58 @@
 package com.smhrd.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.model.MemberDAO;
+import com.smhrd.model.MemberDTO;
+
 public class JoinService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		System.out.println("[JoinService]");
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String m_Id = request.getParameter("m_Id");
+		String m_Pw = request.getParameter("m_Pw");
+		String m_Name = request.getParameter("m_Name");
+		String m_Nickname = request.getParameter("m_Nickname");
+		String m_Phone = request.getParameter("m_Phone");
+		
+		
+		System.out.println(m_Id);
+		System.out.println(m_Pw);
+		System.out.println(m_Name);
+		System.out.println(m_Nickname);
+		System.out.println(m_Phone);
+		
+		MemberDTO dto = new MemberDTO(m_Id, m_Pw, m_Name, m_Nickname, m_Phone);
+		
+		int row = new MemberDAO().join(dto);
+		
+		// 결과값 확인 -> 페이지이동
+		String moveURL = null;
+		if(row > 0) {
+			System.out.println("회원가입 성공");
+			moveURL = "./JoinSuccess.jsp";
+			request.setAttribute("m_Id", m_Id);
+			
+		}else {
+			System.out.println("회원가입 실패");
+			moveURL = "./Main.jsp";
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(moveURL);
+		rd.forward(request, response);
+		
+		
+		
 	}
 
 }
--- src/main/java/com/smhrd/model/MemberDTO.java
+++ src/main/java/com/smhrd/model/MemberDTO.java
@@ -3,6 +3,7 @@ package com.smhrd.model;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
 public class MemberDTO {
-	 // 회원 아이디 
+	
+
+// 회원 아이디 
    @NonNull
 	private String m_Id;
 
@@ -20,16 +23,21 @@ public class MemberDTO {
     @NonNull
     private String m_Pw;
 
+    
     // 회원 이름 
+   
     private String m_Name;
 
     // 회원 닉네임 
+   
     private String m_Nickname;
 
     // 회원 연락처 
+    
     private String m_Phone;
 
     // 회원 가입일자 
+   
     private Date m_Joindate;
 
     // 회원 프로필 
@@ -41,6 +49,16 @@ public class MemberDTO {
     // 회원 유형 
     private String m_Type;
     
+    // 회원가입 DTO
+   
+    @Builder
+    public MemberDTO(String m_Id, String m_Pw, String m_Name, String m_Nickname, String m_Phone ) {
+    	this.m_Id = m_Id;
+    	this.m_Pw = m_Pw;
+    	this.m_Name = m_Name;
+    	this.m_Nickname = m_Nickname;
+    	this.m_Phone = m_Phone;
+    }
    
     
     
