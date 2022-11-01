776d59d5c24514bab54d854568bb3ec084abc830
smhrd <smhrd@DESKTOP-TCH56SA>
2022-11-01 14:52:19 +0900

    Merge branch 'master' of https://github.com/2022-SMHRD-SW-DataDesign-1/hr-hr.git

--- src/main/java/com/smhrd/model/MemberDAO.java
+++ src/main/java/com/smhrd/model/MemberDAO.java
@@ -1,5 +1,33 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
 public class MemberDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+
+	public int join(MemberDTO dto) {
+		System.out.println("조인성공");
+
+		SqlSession session = sqlSessionFactory.openSession(true);
+
+		int row = session.insert("join", dto);
+		session.close();
+
+		return row;
+
+	}
+
+	public MemberDTO login(MemberDTO dto) {
+
+		SqlSession session = sqlSessionFactory.openSession(true);
+
+		MemberDTO info = session.selectOne("login", dto); // 받아온 객체가 MemberDTO 타입으로 sql 문장에서 id가 "login" 문장을 실행한다.
+		session.close();
+
+		return info;
+	}
 
 }
--- src/main/java/com/smhrd/model/MemberDTO.java
+++ src/main/java/com/smhrd/model/MemberDTO.java
@@ -1,5 +1,48 @@
 package com.smhrd.model;
 
-public class MemberDTO {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+
+public class MemberDTO {
+	 // 회원 아이디 
+   @NonNull
+	private String mId;
+
+    // 회원 비밀번호 
+    @NonNull
+    private String mPw;
+
+    // 회원 이름 
+    private String mName;
+
+    // 회원 닉네임 
+    private String mNickname;
+
+    // 회원 연락처 
+    private String mPhone;
+
+    // 회원 가입일자 
+    private Date mJoindate;
+
+    // 회원 프로필 
+    private String mProfile;
+
+    // 회원 포인트 
+    private Double mPoint;
+
+    // 회원 유형 
+    private String mType;
+    
+   
+    
+    
+    
 }
