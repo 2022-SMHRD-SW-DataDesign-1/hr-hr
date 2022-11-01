3ff1d494c2f120aecff7c0ffa298f07abd923d7e
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-01 14:44:27 +0900

    id중복체크일부작성22.11.01

--- src/main/java/com/smhrd/controller/IDCheckService.java
+++ src/main/java/com/smhrd/controller/IDCheckService.java
@@ -1,6 +1,8 @@
 package com.smhrd.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,20 @@ public class IDCheckService extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// 입력한 아이디
+		String inputID = request.getParameter("입력아이디");
+		
+		// dao idcheck()호출
+		MemberDAO dao = new MemberDAO();
+		boolean IDCheck = dao.IDCheck(inputID);
+		
+		// 중복체크 확인(T/F) 결과 넘겨줌
+		PrintWriter out = response.getWriter();
+		out.print(IDCheck);
+		
+		
+		
+		
 	}
 
 }
