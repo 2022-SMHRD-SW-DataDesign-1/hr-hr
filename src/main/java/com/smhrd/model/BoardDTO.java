210723a3c51029e7447ed5097691f7676c91c357
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-01 15:10:17 +0900

    boardDTO임시작성

--- src/main/java/com/smhrd/model/BoardDTO.java
+++ src/main/java/com/smhrd/model/BoardDTO.java
@@ -1,5 +1,25 @@
 package com.smhrd.model;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class BoardDTO {
 
+	private int b_num;
+	private String b_title;
+	private String b_writer;
+	private String b_content;
+	private String b_filename;
+	private Date b_date;
+	private int b_likes;
+	private int b_cnt;
+	private int b_point;
+	
+	
 }
