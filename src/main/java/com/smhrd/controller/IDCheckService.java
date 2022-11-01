b6a60ffeb132565063d6d130bfae40349ac082cf
smhrd <smhrd@DESKTOP-CM2OR85>
2022-11-01 16:29:00 +0900

    IDCheck conflic commit - 11/01김하랑

--- src/main/java/com/smhrd/controller/IDCheckService.java
+++ src/main/java/com/smhrd/controller/IDCheckService.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.model.MemberDAO;
+
 public class IDCheckService extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
