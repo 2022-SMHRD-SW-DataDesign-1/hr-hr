91affedc56ad682f646b9f02a8a51a3ae406076f
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-02 14:34:18 +0900

    22.11.02 block,board dao,dto작성 상현

--- src/main/java/com/smhrd/model/BlockDAO.java
+++ src/main/java/com/smhrd/model/BlockDAO.java
@@ -1,5 +1,29 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
 public class BlockDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+
+	// 차단
+	public int block(BlockDTO dto) {
+		SqlSession session = sqlSessionFactory.openSession(true);
+		int row = session.insert("block", dto);
+		session.close();
+
+		return row;
+	}
+
+	// 차단 해제
+	public int unBlock(BlockDTO dto) {
+		SqlSession session = sqlSessionFactory.openSession(true);
+		int row = session.delete("unBlcok", dto);
+		session.close();
+
+		return row;
+	}
 
 }
--- src/main/java/com/smhrd/model/BoardDAO.java
+++ src/main/java/com/smhrd/model/BoardDAO.java
@@ -1,5 +1,20 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
 public class BoardDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+
+	// 업로드
+	public int upload(BoardDTO dto) {
+		SqlSession session = sqlSessionFactory.openSession(true);
+		int row = session.insert("upload", dto);
+		session.close();
+
+		return row;
+	}
 
 }
