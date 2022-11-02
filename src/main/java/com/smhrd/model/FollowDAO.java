b9f1d74594cba4c8a2446b55168f268cb0d3ed4b
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-02 16:02:06 +0900

    follow,block mapper dao dto 22.11.02상현

--- src/main/java/com/smhrd/model/FollowDAO.java
+++ src/main/java/com/smhrd/model/FollowDAO.java
@@ -1,5 +1,27 @@
 package com.smhrd.model;
 
-public class FollowDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
+import com.smhrd.db.SqlSessionManager;
+
+public class FollowDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	// 팔로우
+		public int follow(BlockDTO dto) {
+			SqlSession session = sqlSessionFactory.openSession(true);
+			int row = session.insert("block", dto);
+			session.close();
+
+			return row;
+		}
+
+		// 팔로우 해제
+		public int unFollow(BlockDTO dto) {
+			SqlSession session = sqlSessionFactory.openSession(true);
+			int row = session.delete("unBlcok", dto);
+			session.close();
+
+			return row;
+		}
 }
