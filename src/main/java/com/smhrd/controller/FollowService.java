58a5df655d9844fc014f242b1f14fc070c95a8ed
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-08 15:24:11 +0900

    않이이개왜안되? 11.08 상현

--- src/main/java/com/smhrd/controller/FollowService.java
+++ src/main/java/com/smhrd/controller/FollowService.java
@@ -15,30 +15,22 @@ public class FollowService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		int follow_cnt = Integer.parseInt(request.getParameter("follow_cnt"));
+		int Follow_cnt = Integer.parseInt(request.getParameter("Follow_cnt"));
 		String m_id = request.getParameter("m_id");
 		String follow_id = request.getParameter("follow_id");
-		System.out.println("팔로우 여부 :"+ follow_cnt);
-		System.out.println("내 아이디 : "+m_id);
-		System.out.println("팔로우한 아이디 : "+follow_id);
 
-		// dao생성
 		FollowDAO dao = new FollowDAO();
-		
-		// dto생성
 		FollowDTO dto = new FollowDTO(m_id, follow_id);
+		PrintWriter out = response.getWriter();
 		
 		// 팔로우 여부에 따라 언팔로우, 팔로우 기능 호출 
-		if (follow_cnt > 0) {
-			System.out.println("언팔 메소드 먹나?");
+		if (Follow_cnt > 0) {
 			dao.unFollow(dto);
-			System.out.println("언팔");
+			out.print(false);
 		} else {
-			System.out.println("팔 메소드 먹나?");
 			dao.follow(dto);
-			System.out.println("팔");
+			out.print(true);
 		}
-		response.sendRedirect("./testAll2.jsp");
 	}
 
 }
