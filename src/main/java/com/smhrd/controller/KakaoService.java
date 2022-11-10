4ae6cae7f9f3844c487f2ebadde6b530e03a0c20
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-11-10 10:55:18 +0900

    ?

--- src/main/java/com/smhrd/controller/KakaoService.java
+++ src/main/java/com/smhrd/controller/KakaoService.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
 import com.smhrd.model.MemberDAO;
 import com.smhrd.model.MemberDTO;
 
